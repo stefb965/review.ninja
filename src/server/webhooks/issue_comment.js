dcaea7a945d17f729b7702c100932eafa063a6e2
David Farr <david.farr@sap.com>
2015-12-15 15:34:07 -0800

    Add webhook middleware
    When we get a webhook request from github, we will now retrieve
    the token saved in our repo collection. The middleware adds this
    to req.args.token

--- src/server/webhooks/issue_comment.js
+++ src/server/webhooks/issue_comment.js
@@ -2,13 +2,12 @@
 
 // models
 var Star = require('mongoose').model('Star');
-var User = require('mongoose').model('User');
 
 // services
-var github = require('../services/github');
-var status = require('../services/status');
 var star = require('../services/star');
 var flags = require('../services/flags');
+var github = require('../services/github');
+var status = require('../services/status');
 var notification = require('../services/notification');
 
 //////////////////////////////////////////////////////////////////////////////////////////////
@@ -19,49 +18,43 @@ module.exports = function(req, res) {
 
     var user = req.args.repository.owner.login;
     var repo = req.args.repository.name;
+    var token = req.args.token;
     var number = req.args.issue.number;
     var sender = req.args.sender;
     var comment = req.args.comment.body;
     var repo_uuid = req.args.repository.id;
 
-    User.findOne({ _id: req.params.id }, function(err, ninja) {
+    var actions = {
+        created: function() {
 
-        if(err || !ninja) {
-            return res.status(404).send('User not found');
-        }
+            //
+            // Add ninja star
+            //
 
-        var actions = {
-            created: function() {
+            var func = flags.unstar(comment) ? 'remove' : flags.star(comment) ? 'create' : null;
 
-                //
-                // Add ninja star
-                //
-
-                var func = flags.unstar(comment) ? 'remove' : flags.star(comment) ? 'create' : null;
-
-                if(func) {
-                    github.call({
-                        obj: 'pullRequests',
-                        fun: 'get',
-                        arg: {
-                            user: user,
-                            repo: repo,
-                            number: number
-                        },
-                        token: ninja.token
-                    }, function(err, pull) {
-                        if(!err) {
-                            star[func](pull.head.sha, user, repo, repo_uuid, number, sender, ninja.token);
-                        }
-                    });
-                }
+            if(func) {
+                github.call({
+                    obj: 'pullRequests',
+                    fun: 'get',
+                    arg: {
+                        user: user,
+                        repo: repo,
+                        number: number
+                    },
+                    token: token
+                }, function(err, pull) {
+                    if(!err) {
+                        star[func](pull.head.sha, user, repo, repo_uuid, number, sender, token);
+                    }
+                });
             }
-        };
-
-        if (actions[req.args.action]) {
-            actions[req.args.action]();
         }
+    };
 
-        res.end();
-    });
+    if (actions[req.args.action]) {
+        actions[req.args.action]();
+    }
+
+    res.end();
 };
--- src/server/webhooks/status.js
+++ src/server/webhooks/status.js
@@ -1,5 +1,5 @@
 'use strict';
-// respond to ping event
+
 module.exports = function(req, res) {
     res.end();
 };
