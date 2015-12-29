5b3dfd76a05e25b657454f79e5e5a4ed5da838bc
David Farr <david.farr@sap.com>
2015-12-29 12:18:07 -0500

    Drop duplicates when ensuring unique key in migrations

--- src/server/migrations/1.5.0_1.js
+++ src/server/migrations/1.5.0_1.js
@@ -3,5 +3,5 @@ exports.id = '1.5.0_1';
 
 exports.up = function (done) {
     var Repo = this.db.collection('repos');
-    Repo.ensureIndex({repo: 1}, {unique: true}, done);
+    Repo.ensureIndex({repo: 1}, {unique: true, dropDups: true}, done);
 };
--- src/server/migrations/1.5.0_2.js
+++ src/server/migrations/1.5.0_2.js
@@ -3,5 +3,5 @@ exports.id = '1.5.0_2';
 
 exports.up = function (done) {
     var Settings = this.db.collection('settings');
-    Settings.ensureIndex({user: 1, repo: 1}, {unique: true}, done);
+    Settings.ensureIndex({user: 1, repo: 1}, {unique: true, dropDups: true}, done);
 };
