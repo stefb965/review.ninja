18c2c64328d9fd04124ddf13df718c5fd5fc4b48
David Farr <david.farr@sap.com>
2016-07-18 13:29:04 -0700

    Add external API call to update review comment

--- src/client/controller/pull.js
+++ src/client/controller/pull.js
@@ -167,7 +167,19 @@ module.controller('PullCtrl', [
         $scope.updateReviewComment = function(comment, ref) {
             if(comment && comment.body) {
 
-                console.log(comment, comment.id);
+                $HUB.call('pullRequests', 'updateComment', {
+                    user: $stateParams.user,
+                    repo: $stateParams.repo,
+                    number: comment.id,
+                    body: comment.body
+                }, function(err, c) {
+                    if(!err) {
+                        Markdown.html(c.value.body, function(html) {
+                            comment.edit = false;
+                            comment.html = html;
+                        });
+                    }
+                });
 
             }
         };
